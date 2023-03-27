a85e49568d8dbff32d81a0055840a01b91ea03c4
OrangeSpacer <maxaev.art@yandex.ru>
2023-03-27 23:20:36 +0300

    feat: add inversify

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,13 +5,19 @@ import { DatabaseService } from "./database/db.service";
 import { DotenvConfigOutput, config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { injectable, inject } from "inversify";
+import "reflect-metadata";
 
+@injectable()
 export class App {
 	private port: number;
 	private app: Express;
 	private config: DotenvConfigOutput;
 
-	constructor(private readonly logger: LoggerService, private databse: DatabaseService) {
+	constructor(
+		@inject("Logger") private logger: LoggerService,
+		@inject("Database") private database: DatabaseService,
+	) {
 		this.port = 5000;
 		this.app = express();
 		this.config = config();
@@ -31,7 +37,7 @@ export class App {
 	public async init(): Promise<void> {
 		this.app.listen(this.port);
 		this.logger.info(`[App] Сервер работает на htpp://127.0.0.1:${this.port}`);
-		await this.databse.connect();
+		await this.database.connect();
 		this.useConfig();
 		this.useRoutes();
 	}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
+import { Container, ContainerModule, interfaces } from "inversify";
 import { App } from "./app";
 import { DatabaseService } from "./database/db.service";
 import { LoggerService } from "./logger/logger.service";
 
-async function bootstrap(): Promise<void> {
-	const app = new App(new LoggerService(), new DatabaseService(new LoggerService()));
+const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<LoggerService>("Logger").to(LoggerService).inSingletonScope();
+	bind<DatabaseService>("Database").to(DatabaseService).inSingletonScope();
+	bind<App>("App").to(App);
+});
+
+async function bootstrap(): Promise<object> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>("App");
 	await app.init();
+	return { appContainer, app };
 }
 
 void bootstrap();
