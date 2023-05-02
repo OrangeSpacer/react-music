bc3ae99b59a9d5a867e93ed52be5513fe2214726
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-02 21:31:17 +0300

    feat: add api for get all music

--- frontend/src/store/api/music.api.ts
+++ frontend/src/store/api/music.api.ts
@@ -1,11 +1,11 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-import { IMusic } from "../../types/music.interface"
+import { IMusicData } from "../../types/music.interface"
 
 export const musicApi = createApi({
     reducerPath: "musicApi",
     baseQuery: fetchBaseQuery({baseUrl: "http://127.0.0.1:5000/track"}),
     endpoints: (builder) => ({
-        getMusicAll: builder.query<IMusic[], string>({
+        getMusicAll: builder.query<IMusicData[], string>({
             query: () => "/all"
         })
     })
