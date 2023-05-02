6d0c65ef5a1a7b1dde799f79ddc842ec5d58cced
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-02 22:42:30 +0300

    refactor: made minor visual changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import Burger from "./components/Burger/Burger"
 import Main from "./pages/Main/Main"
 
 const App = () => {
-  const links = [
-    {text: "test", link: '/test'}
-  ]
   return (
     <div>
       <Main/>
