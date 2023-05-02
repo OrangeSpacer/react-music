92f2aee85920cf801cec316eec22b6944809d2c8
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-02 22:32:20 +0300

    feat: add music page

---
+++ frontend/src/pages/Musics/Musics.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export interface MusicsProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    
+}
---
+++ frontend/src/pages/Musics/Musics.tsx
@@ -0,0 +1,37 @@
+import Music from "../../components/Music/Music"
+import Button from "../../components/UI/Button/Button"
+import Title from "../../components/UI/Title/Title"
+import { useGetMusicAllQuery } from "../../store/api/music.api"
+
+import styles from "./Music.module.scss"
+
+const Musics = () => {
+    const {data, error, isLoading} = useGetMusicAllQuery("")
+
+    if(isLoading) {
+      return <h1>Загрузка</h1>
+    }
+  
+    if(error){
+      return error
+    }
+  return (
+    <div>
+        <div className={styles.titleBlock}>
+            <div>
+                <Title text="All music"/>
+            </div>
+            <div>
+                <Button func={() => console.log("play music")} typeView="circle">
+                    Play
+                </Button>
+            </div>
+        </div>
+        <div className={styles.musicBlock}>
+            {data?.map(music => <Music key={music._id} musicData={music}/>)}
+        </div>
+    </div>
+  )
+}
+
+export default Musics
