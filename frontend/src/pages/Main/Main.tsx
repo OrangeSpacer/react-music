7033487dc0fb53f7b80ed85b38db169ad95e2ccc
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-02 21:56:47 +0300

    refactor: replace the component with the outlet for the router

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,19 +1,50 @@
-import React from 'react'
 import Burger from '../../components/Burger/Burger'
-import Content from '../Content/Content'
 import Player from '../../components/Player/Player'
 
 
 import style from "./Main.module.scss";
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
+  const burgerLinks = [
+    {
+      text: "favorites",
+      link: "/favorites",
+      imgLink: "img/burger/favorites.svg"
+    },
+    {
+      text: "playlists",
+      link: "/playlists",
+      imgLink: "img/burger/favorites.svg"
+    },
+    {
+      text: "your playlists",
+      link: "/playlists/local",
+      imgLink: "img/burger/favorites.svg"
+    },
+    {
+      text: "music",
+      link: "/music",
+      imgLink: "img/burger/music.svg"
+    },
+    {
+      text: "create playlist",
+      link: "/create/playlist",
+      imgLink: "img/burger/favorites.svg"
+    },
+    {
+      text: "create music",
+      link: "/create/music",
+      imgLink: "img/burger/favorites.svg"
+    }
+  ]
   return (
     <div className={style.mainBlock}>
         <div className={style.burger}>
-            <Burger listLink={[]}/>
+            <Burger listLink={burgerLinks}/>
         </div>
         <div className={style.content}>
-            <Content/>
+            <Outlet/>
         </div>
         <div className={style.player}>
             <Player/>
