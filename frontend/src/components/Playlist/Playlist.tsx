ab2025eeefdde8ff56009fe80b03fc0b4abea195
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-17 16:58:10 +0300

    feat: update playlist loader and playlist components

--- frontend/src/components/Playlist/Playlist.tsx
+++ frontend/src/components/Playlist/Playlist.tsx
@@ -1,22 +1,25 @@
 import { IPlaylistProps } from "./Playlist.props"
 
 import styles from "./Playlist.module.scss"
+import { Link } from "react-router-dom"
 
 const Playlist = ({id,title,tracksLength}: IPlaylistProps) => {
   return (
-    <div className={styles.playlist}>
-        <div className={styles.imgBlock}>
-            <img src="/img/playlist/playlist.jpg" alt="imgPlaylist" className={styles.img}/>
-        </div>
-        <div className={styles.infoBlock}>
-            <div className={styles.title}>
-                {title}
+    <Link to={id} className={styles.link}>
+        <div className={styles.playlist}>
+            <div className={styles.imgBlock}>
+                <img src="/img/playlist/playlist.jpg" alt="imgPlaylist" className={styles.img}/>
             </div>
-            <div className={styles.tracks}>
-                Tracks count: {tracksLength}
+            <div className={styles.infoBlock}>
+                <div className={styles.title}>
+                    {title}
+                </div>
+                <div className={styles.tracks}>
+                    Tracks count: {tracksLength}
+                </div>
             </div>
         </div>
-    </div>
+    </Link>
   )
 }
 
