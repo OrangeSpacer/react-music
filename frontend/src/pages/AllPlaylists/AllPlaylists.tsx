c8007e3088c5769b67dde06ba26b48028438ce23
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-19 14:25:45 +0300

    feat: add playlists all page

---
+++ frontend/src/pages/AllPlaylists/AllPlaylists.tsx
@@ -0,0 +1,17 @@
+import Loader from '../../components/Loader/Loader'
+import PlaylistBlock from '../../components/PlaylistBlock/PlaylistBlock'
+import { useGetPlaylistAllQuery } from '../../store/api/playlist/playlist.api'
+
+import styles from "./AllPlaylists.module.scss"
+
+const AllPlaylists = () => {
+  const {isLoading,data,} = useGetPlaylistAllQuery("")
+
+  return (
+    <div className={styles.playlists}>
+      {isLoading ? <Loader/>: <PlaylistBlock title='All playlists' fetchData={data}/>}
+    </div>
+  )
+}
+
+export default AllPlaylists
