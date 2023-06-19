00338cac9399ecb88a2ce34f5e5b76603435ad6d
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-19 14:24:52 +0300

    feat: add playlist block component

---
+++ frontend/src/components/PlaylistBlock/PlaylistBlock.props.ts
@@ -0,0 +1,7 @@
+import { IPlaylist } from "../../types/playlist.interface"
+
+
+export interface IPlaylistBlock {
+    title: string
+    fetchData:  IPlaylist[]
+}
---
+++ frontend/src/components/PlaylistBlock/PlaylistBlock.tsx
@@ -0,0 +1,23 @@
+import Title from '../UI/Title/Title'
+
+import styles from "./PlaylistBlock.module.scss"
+import { IPlaylistBlock } from './PlaylistBlock.props'
+import { IPlaylist } from '../../types/playlist.interface'
+import Playlist from '../Playlist/Playlist'
+
+const PlaylistBlock = ({title,fetchData}:IPlaylistBlock) => {
+  return (
+    <div>
+        <Title text={title}/>
+        <div className={styles.content}>
+            <div className={styles.playlists}>
+                <div className={styles.playlists}>
+                        {fetchData?.map((playlist:IPlaylist) => <Playlist id={playlist._id} title={playlist.title} tracksLength={playlist.tracks.length}/>)}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PlaylistBlock
