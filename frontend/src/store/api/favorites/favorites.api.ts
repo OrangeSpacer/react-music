238badd4397f8b153d579516114130d28fd62d3c
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-20 22:07:32 +0300

    feat: add favorites add and delete method in api

--- frontend/src/store/api/favorites/favorites.api.ts
+++ frontend/src/store/api/favorites/favorites.api.ts
@@ -1,12 +1,25 @@
-import { IMusicData } from "../../../types/music.interface";
 import { apiSlice } from "../apiSlice";
 
 export const favoritesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getAllFavorites: builder.query<IMusicData[], string>({
+        getAllFavorites: builder.query<string[], string>({
             query: () => "/favorites/all"
-        })
+        }),
+        addInFavorties: builder.mutation<string, object>({
+            query: (payload) => ({
+                url: "/favorites/add",
+                method: "POST",
+                body: payload
+            })
+        }),
+        deleteInFavorties: builder.mutation<string, object>({
+            query: (payload) => ({
+                url: "/favorites/remove",
+                method: "POST",
+                body: payload
+            })
+        }),
     })
 })
 
-export const {useGetAllFavoritesQuery} = favoritesApi
+export const {useGetAllFavoritesQuery, useAddInFavortiesMutation, useDeleteInFavortiesMutation} = favoritesApi
