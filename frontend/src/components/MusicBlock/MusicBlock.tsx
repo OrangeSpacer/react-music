107c9e8b2783023e37702a6c75c8c388dad3c6c0
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-20 22:06:28 +0300

    feat: add function for add favorites

--- frontend/src/components/MusicBlock/MusicBlock.tsx
+++ frontend/src/components/MusicBlock/MusicBlock.tsx
@@ -3,15 +3,27 @@ import { IMusicBlock } from './MusicBlock.props'
 import Music from '../Music/Music'
 
 import styles from "./MusicBlock.module.scss"
+import { useGetAllFavoritesQuery } from '../../store/api/favorites/favorites.api'
+import Loader from '../Loader/Loader'
 
 const MusicBlock = ({musics,title,addInPlaylist,deleteInPlaylist,deleteMusic}: IMusicBlock) => {
+  const {data: favorites, isSuccess} = useGetAllFavoritesQuery("")
+
+  const handleCheckFavorties = (id:string):boolean => {
+    console.log(id)
+    if(isSuccess && favorites?.includes(id)) {
+      return true
+    } else {
+      return false
+    }
+  }
   return (
     <div>
         <div className={styles.titleBlock}>
             <Title text={title}/>
         </div>
         <div className={styles.musicBlock}>
-            {musics.map(music => <Music key={music._id} musicData={music} addInPlaylist={addInPlaylist} deleteInPlaylist={deleteInPlaylist} deleteMusic={deleteMusic}/>)}
+            {isSuccess ?  musics.map(music => <Music key={music._id} musicData={music} isFavorties={handleCheckFavorties(music._id)} addInPlaylist={addInPlaylist} deleteInPlaylist={deleteInPlaylist} deleteMusic={deleteMusic}/>): <Loader/>}
         </div>
     </div>
   )
