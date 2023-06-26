46df1f4b269949813d91636dc0d94ff6a000cb78
OrangeSpacer <maxaev.art@yandex.ru>
2023-06-26 23:00:15 +0300

    feat: add function for remove favorites music in favorites page

--- frontend/src/pages/Favorites/Favorites.tsx
+++ frontend/src/pages/Favorites/Favorites.tsx
@@ -1,17 +1,32 @@
 import { useEffect } from 'react'
-import { useGetAllFavoritesMutation } from '../../store/api/favorites/favorites.api'
+import { useDeleteInFavortiesMutation, useGetAllFavoritesMutation } from '../../store/api/favorites/favorites.api'
 import Loader from '../../components/Loader/Loader'
 import Title from '../../components/UI/Title/Title'
 import Music from '../../components/Music/Music'
 
 import styles from "./Favorties.module.scss"
+import { useAppDispatch, useAppSelector } from '../../hooks/redux'
+import { deleteFavoritesForIdCheck, setFavorties } from '../../store/features/favorties/favoritesSlice'
 
 const Favorites = () => {
     const [getFavorites,res] = useGetAllFavoritesMutation()
-    
+    const [deleteFavorites] = useDeleteInFavortiesMutation()
+    const dispatch = useAppDispatch()
+    const {favorties} = useAppSelector(state => state.favoritesReducer)
     useEffect(() => {
       getFavorites("")
     },[getFavorites])
+
+    useEffect(() => {
+      if(res.data){
+        dispatch(setFavorties(res.data))
+      }
+    },[dispatch,res.data])
+
+    const handleDeleteFavorites = (id: string) => {
+      deleteFavorites({trackId: id})
+      dispatch(deleteFavoritesForIdCheck(id))
+    }
 
   return (
     <div>
@@ -22,7 +37,7 @@ const Favorites = () => {
               <Title text='Favorites'/>
             </div>
             <div className={styles.musicBlock}>
-              {res.data && res.data.map(music => <Music key={music._id} musicData={music}/>)}
+              {res.data && favorties && favorties.map(music => <Music key={music._id} musicData={music} isFavorites={true} deleteFavorites={() => handleDeleteFavorites(music._id)}/>)}
             </div>
           </div>:
           null
