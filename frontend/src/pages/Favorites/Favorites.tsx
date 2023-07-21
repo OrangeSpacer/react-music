395c6409ec93905c033cacec3c78a0d382bea53d
OrangeSpacer <maxaev.art@yandex.ru>
2023-07-21 17:29:02 +0300

    feat: add music func in favorites page

--- frontend/src/pages/Favorites/Favorites.tsx
+++ frontend/src/pages/Favorites/Favorites.tsx
@@ -7,12 +7,14 @@ import Music from '../../components/Music/Music'
 import styles from "./Favorties.module.scss"
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
 import { deleteFavoritesForIdCheck, setFavorties } from '../../store/features/favorties/favoritesSlice'
+import { pauseMusic, playMusic, setPlayerTracks } from '../../store/features/player/playerSlice'
 
 const Favorites = () => {
     const [getFavorites,res] = useGetAllFavoritesMutation()
     const [deleteFavorites] = useDeleteInFavortiesMutation()
     const dispatch = useAppDispatch()
     const {favorties} = useAppSelector(state => state.favoritesReducer)
+    const {currentTrack,isPlaying,tracks} = useAppSelector(state => state.playerReducer)
     useEffect(() => {
       getFavorites("")
     },[getFavorites])
@@ -28,6 +30,17 @@ const Favorites = () => {
       dispatch(deleteFavoritesForIdCheck(id))
     }
 
+    const handleSetMusics = () => {
+      if(favorties != null && (tracks.length == 0 || favorties[0].title != tracks[0]?.title)){
+        dispatch(setPlayerTracks(favorties))
+      }
+      dispatch(playMusic())
+    }
+  
+    const handlePauseMusics = () => {
+      dispatch(pauseMusic())
+    }
+
   return (
     <div>
         {res.isLoading ? <Loader/>: null}
@@ -37,7 +50,7 @@ const Favorites = () => {
               <Title text='Favorites'/>
             </div>
             <div className={styles.musicBlock}>
-              {res.data && favorties && favorties.map(music => <Music key={music._id} musicData={music} isFavorites={true} deleteFavorites={() => handleDeleteFavorites(music._id)}/>)}
+              {res.data && favorties && favorties.map(music => <Music id={music._id} isLocal={false} isPlaying={music._id == currentTrack?._id && isPlaying}  key={music._id} playMusic={handleSetMusics} pauseMusic={handlePauseMusics} musicData={music} isFavorites={true} deleteFavorites={() => handleDeleteFavorites(music._id)}/>)}
             </div>
           </div>:
           null
